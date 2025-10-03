9dcf015ac78d6e42cc8a21a39239f38d50e24909
nznnnks <evilrobbitop2006@gmail.com>
2025-10-03 19:40:20 +0300

    Start project

---
+++ src/main/java/com/travelagency/travel_microservices/TravelMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.travelagency.travel_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelMicroservicesApplication.class, args);
+	}
+
+}
